89585511604bb3a118a492c4493a5f25e76eb273
Marzieh <marzieh.tajik@zoodfood.com>
2021-10-07 19:02:41 +0330

    add credit column to users

--- app/User.php
+++ app/User.php
@@ -19,7 +19,7 @@ class User extends Authenticatable implements JWTSubject
      * @var array
      */
     protected $fillable = [
-        'username', 'email', 'password', 'bio', 'image'
+        'username', 'email', 'password', 'bio', 'image', 'credit'
     ];
 
     /**
