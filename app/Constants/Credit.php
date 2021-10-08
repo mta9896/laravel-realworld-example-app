a2788b310f035d20a3cd1a6992f16811597e9730
Marzieh <marzieh.tajik@zoodfood.com>
2021-10-08 20:23:31 +0330

    add user credit service

--- app/Constants/Credit.php
+++ app/Constants/Credit.php
@@ -10,12 +10,6 @@ class Credit
 {
     const PRIMARY_CREDIT = 100000;
 
-    const CREDIT_PER_ARTICLE = 5000;
-
-    const CREDIT_PER_COMMENT = 5000;
-
-    const FREE_COMMENTS_COUNT_THRESHOLD = 5;
-
     const NOTIFY_USER_CREDIT_THRESHOLD = 20000;
 
     const BAN_USER_CREDIT_THRESHOLD = 0;
---
+++ app/Services/CreditActionService.php
@@ -0,0 +1,34 @@
+<?php
+
+
+namespace App\Services;
+
+
+use App\Constants\Credit;
+use App\Services\Notification\NotifyUserInterface;
+use App\User;
+use Carbon\Carbon;
+
+class CreditActionService
+{
+    private $notifyUser;
+
+    public function __construct(NotifyUserInterface $notifyUser)
+    {
+        $this->notifyUser = $notifyUser;
+    }
+
+    public function handleCreditUpdateActions(User $user, int $primaryCredit, int $updatedCredit)
+    {
+        if ($updatedCredit > Credit::NOTIFY_USER_CREDIT_THRESHOLD) {
+            return;
+        }
+
+        if ($primaryCredit > Credit::NOTIFY_USER_CREDIT_THRESHOLD && $updatedCredit <= Credit::NOTIFY_USER_CREDIT_THRESHOLD) {
+            $this->notifyUser->notify($user);
+        } elseif ($primaryCredit > Credit::BAN_USER_CREDIT_THRESHOLD && $updatedCredit <= Credit::BAN_USER_CREDIT_THRESHOLD) {
+            $user->deactivated_at = Carbon::now();
+            $user->save();
+        }
+    }
+}
